f60605e0dad55cfca14af371f948aba1e45c9617
JonPlaytv <gamerjon00@gmail.com>
2024-01-02 15:51:55 +0100

    j

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
 from flask import send_from_directory
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+
 
 
 
@@ -44,7 +46,6 @@ def queue_status():
 
     # Return the response from the external server
     return jsonify(response.json())
-
 
 if __name__ == '__main__':
     
