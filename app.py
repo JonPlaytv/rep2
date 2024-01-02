8b8993068bcbd8ffc0f073e35dd228f72b0077e4
JonPlaytv <gamerjon00@gmail.com>
2024-01-02 14:17:28 +0100

    f
    dssdddd

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+from flask import send_from_directory
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+
+
+
 
 @app.route('/generate', methods=['POST'])
 def generate():
