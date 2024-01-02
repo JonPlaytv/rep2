88558acfa5b81845ec1321bd8157faf4c8a557a0
JonPlaytv <gamerjon00@gmail.com>
2024-01-02 17:03:13 +0100

    dd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
+import os
 import requests
 
 app = Flask(__name__)
@@ -6,38 +7,43 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-from flask import send_from_directory
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-
-
-
-
-
 @app.route('/generate', methods=['POST'])
 def generate():
-    # Get user input from the request
-    user_text = request.json.get('prompt')
-    width = request.json.get('width')
-    height = request.json.get('height')
-    negative_prompt = request.json.get('negative_prompt')  # Added line for negative prompt
+    try:
+        # Get user input from the request
+        user_text = request.json.get('prompt')
+        width = request.json.get('width')
+        height = request.json.get('height')
+        negative_prompt = request.json.get('negative_prompt')
+        steps = request.json.get('steps')  # Use request.json instead of request.form
 
-    # Make a request to your FastAPI endpoint
-    response = requests.post('http://37.60.173.43:8080/sdapi/v1/txt2img', json={
-        'text': user_text,
-        'width': width,
-        'height': height,
-        'negative_prompt': negative_prompt  # Added line for negative prompt
-    })
+        # Make a request to your FastAPI endpoint
+        response = requests.post('http://37.60.173.43:8080/sdapi/v1/txt2img', json={
+            'text': user_text,
+            'width': width,
+            'height': height,
+            'negative_prompt': negative_prompt,
+            'steps': steps,
+        })
 
-    # Assume the API responds with an image encoded in base64
-    base64_image = response.json().get('images')
+        # Check if the request was successful (status code 200)
+        response.raise_for_status()
 
-    return jsonify({'images': base64_image})
+        # Assume the API responds with an image encoded in base64
+        base64_image = response.json().get('images')
 
+        return jsonify({'images': base64_image})
+
+    except requests.RequestException as e:
+        return jsonify({'error': f'Error making API request: {e}'}), 500
+
+    except Exception as e:
+        return jsonify({'error': f'An unexpected error occurred: {e}'}), 500
 
 @app.route('/queue/status')
 def queue_status():
@@ -48,5 +54,4 @@ def queue_status():
     return jsonify(response.json())
 
 if __name__ == '__main__':
-    
-        app.run(debug=True)
+    app.run(debug=True)
