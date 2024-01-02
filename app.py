73267ca84259558cf09df7deebce208bfdbad1c7
JonPlaytv <83393169+JonPlaytv@users.noreply.github.com>
2024-01-02 21:46:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ if __name__ == '__main__':
     # app.run(debug=True, host="0.0.0.0", port="5000")
     # Production
     http_server = WSGIServer(('0.0.0.0', 5000), app)
-    http_server.serve_forever()
+    
